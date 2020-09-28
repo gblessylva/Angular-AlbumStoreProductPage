b18a720a06b8df952b5a112865c4a8b69eb67774
gblessylva <gblessylva@gmail.com>
2020-09-28 14:27:20 +0100

    album type added

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -7,10 +7,11 @@ import { ProductService} from '../product.service'
   styleUrls: ['./product-tracklisting.component.css']
 })
 export class ProductTracklistingComponent implements OnInit {
-  
-  constructor() { }
+  albumInfo : Album;
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response =>this.albumInfo = response)
   }
 
 }
